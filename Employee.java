aea36e49eaa76ecc678ec0a16688176a9c2cc880
Gorrelaraviteja <163235190+Gorrelaraviteja@users.noreply.github.com>
2024-11-09 03:42:58 -0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Employee{
+public static void main (String args[]){
+Scanner sc=new scanner(System.in)
+	int emp_number;
+	String emp_name;
+	int emp_age;
+	System.out.Println( "enter a employee number");
+	number=sc.nextInt();
+	System.out.Println( "enter a employee name");
+	name=sc.next();
+	System.out.Println( "enter a employee age");
+	n=sc.nextInt();
+System.out.Println( "enter a employee number:" +emp_number);
+System.out.Println( "enter a employee name:" +emp_name);
+System.out.Println( "enter a employee age:" +emp_age);
+
+}
+	
+}
---
+++ Ravi.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Ravi
+{
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to ravi programs");
+	}
+}
