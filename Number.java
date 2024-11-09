1a8c8a00df9254f86a9b6a0f279c385ee9a01c59
Gorrelaraviteja <163235190+Gorrelaraviteja@users.noreply.github.com>
2024-11-09 04:37:57 -0800

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Number
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a=16;
+		if(a>0)
+		{
+			System.out.println("a is positive:");
+
+		}
+		else{
+			System.out.println("a is not postive:");
+		}
+	
+	}
+}
